8d4df04c272877f92748caa652839b4863f6624c
Lázaro Silva <lazhcsilva@gmail.com>
2025-06-17 00:38:35 -0300

    Start project

---
+++ back/src/main/java/com/interprisedealership/InterprisedealershipApplication.java
@@ -0,0 +1,13 @@
+package com.interprisedealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterprisedealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterprisedealershipApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/interprisedealership/InterprisedealershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interprisedealership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterprisedealershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
